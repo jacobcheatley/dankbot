2e57ceccc85088fccb2fa34d9ed58dbe2f31c196
jacobcheatley <jacobcheatley@gmail.com>
2016-04-28 17:29:53 +1200

    Extended config functionality

--- cogs/utils/config.py
+++ cogs/utils/config.py
@@ -1,2 +1,16 @@
 owner_id = '132448107148476417'
 spam_channels = ['spam_allowed', 'spam', 'twitch_chat']
+initial_extensions = [
+    'cogs.admin',
+    'cogs.conversation',
+    'cogs.fun',
+    'cogs.info',
+    'cogs.replies',
+    'cogs.tags',
+    'cogs.queries'
+]
+bot_kwargs = {
+    'command_prefix': '!',
+    'description': 'This bot is Dank.',
+    'pm_help': False
+}
